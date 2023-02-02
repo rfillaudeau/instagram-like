f0d86cf5cc5cd6bc99895c72e4496c81c03b4058
Romain Fillaudeau <r.fillaudeau@gmail.com>
2023-02-02 11:43:21 +0100

    Fixed unit tests

--- tests/Unit/Serializer/PostNormalizerTest.php
+++ tests/Unit/Serializer/PostNormalizerTest.php
@@ -4,7 +4,6 @@ namespace App\Tests\Unit\Serializer;
 
 use App\Entity\Post;
 use App\Entity\User;
-use DateTime;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
@@ -21,16 +20,11 @@ class PostNormalizerTest extends KernelTestCase
 
         $container = static::getContainer();
 
-        $createdAt = new DateTime('2022-12-13 16:17:18');
-        $updatedAt = new DateTime('2022-12-22 18:19:20');
-
         $user = UserNormalizerTest::makeUser();
 
         $post = (new Post())
             ->setDescription('Test description')
             ->setPictureFilename('my_picture.jpg')
-            ->setCreatedAt($createdAt)
-            ->setUpdatedAt($updatedAt)
             ->setLikeCount(15)
             ->setUser($user);
 
@@ -44,33 +38,9 @@ class PostNormalizerTest extends KernelTestCase
             ]]
         );
 
-        $this->assertEquals([
-            'id' => null,
-            'description' => 'Test description',
-            'pictureFilename' => 'my_picture.jpg',
-            'pictureFilepath' => '/uploads/posts/my_picture.jpg',
-            'isLiked' => false,
-            'createdAt' => '2022-12-13T16:17:18+00:00',
-            'updatedAt' => '2022-12-22T18:19:20+00:00',
-            'user' => [
-                'id' => null,
-                'username' => 'test_username',
-                'email' => 'test_email@test.com',
-                'avatarFilename' => 'my_avatar.jpg',
-                'postCount' => 25,
-                'followingCount' => 70,
-                'followerCount' => 9,
-                'isFollowed' => false,
-                'avatarFilepath' => '/uploads/avatars/my_avatar.jpg',
-                'bio' => 'Hello there',
-            ],
-            'likeCount' => 15
-        ], $result);
-
         $this->assertArrayHasKey('id', $result);
         $this->assertArrayHasKey('description', $result);
-        $this->assertArrayHasKey('pictureFilename', $result);
-        $this->assertArrayHasKey('pictureFilepath', $result);
+        $this->assertArrayHasKey('pictureFilePath', $result);
         $this->assertArrayHasKey('isLiked', $result);
         $this->assertArrayHasKey('likeCount', $result);
         $this->assertArrayHasKey('createdAt', $result);
@@ -79,31 +49,26 @@ class PostNormalizerTest extends KernelTestCase
         $this->assertArrayHasKey('id', $result['user']);
         $this->assertArrayHasKey('username', $result['user']);
         $this->assertArrayHasKey('email', $result['user']);
-        $this->assertArrayHasKey('avatarFilename', $result['user']);
         $this->assertArrayHasKey('postCount', $result['user']);
         $this->assertArrayHasKey('followingCount', $result['user']);
         $this->assertArrayHasKey('followerCount', $result['user']);
         $this->assertArrayHasKey('isFollowed', $result['user']);
-        $this->assertArrayHasKey('avatarFilepath', $result['user']);
+        $this->assertArrayHasKey('avatarFilePath', $result['user']);
         $this->assertArrayHasKey('bio', $result['user']);
         $this->assertArrayHasKey('id', $result['user']);
 
         $this->assertEquals('Test description', $result['description']);
-        $this->assertEquals('my_picture.jpg', $result['pictureFilename']);
-        $this->assertEquals('/uploads/posts/my_picture.jpg', $result['pictureFilepath']);
+        $this->assertEquals('/uploads/posts/my_picture.jpg', $result['pictureFilePath']);
         $this->assertEquals(false, $result['isLiked']);
-        $this->assertEquals('2022-12-13T16:17:18+00:00', $result['createdAt']);
-        $this->assertEquals('2022-12-22T18:19:20+00:00', $result['updatedAt']);
         $this->assertEquals(15, $result['likeCount']);
 
         $this->assertEquals('test_username', $result['user']['username']);
         $this->assertEquals('test_email@test.com', $result['user']['email']);
-        $this->assertEquals('my_avatar.jpg', $result['user']['avatarFilename']);
         $this->assertEquals(25, $result['user']['postCount']);
         $this->assertEquals(70, $result['user']['followingCount']);
         $this->assertEquals(9, $result['user']['followerCount']);
         $this->assertEquals(false, $result['user']['isFollowed']);
-        $this->assertEquals('/uploads/avatars/my_avatar.jpg', $result['user']['avatarFilepath']);
+        $this->assertEquals('/uploads/avatars/my_avatar.jpg', $result['user']['avatarFilePath']);
         $this->assertEquals('Hello there', $result['user']['bio']);
     }
 }
--- tests/Unit/Serializer/UserNormalizerTest.php
+++ tests/Unit/Serializer/UserNormalizerTest.php
@@ -33,24 +33,33 @@ class UserNormalizerTest extends KernelTestCase
         $this->assertArrayHasKey('id', $result);
         $this->assertArrayHasKey('username', $result);
         $this->assertArrayHasKey('email', $result);
-        $this->assertArrayHasKey('avatarFilename', $result);
         $this->assertArrayHasKey('postCount', $result);
         $this->assertArrayHasKey('followingCount', $result);
         $this->assertArrayHasKey('followerCount', $result);
         $this->assertArrayHasKey('isFollowed', $result);
-        $this->assertArrayHasKey('avatarFilepath', $result);
+        $this->assertArrayHasKey('avatarFilePath', $result);
         $this->assertArrayHasKey('bio', $result);
-        $this->assertArrayHasKey('id', $result);
 
         $this->assertEquals('test_username', $result['username']);
         $this->assertEquals('test_email@test.com', $result['email']);
-        $this->assertEquals('my_avatar.jpg', $result['avatarFilename']);
         $this->assertEquals(25, $result['postCount']);
         $this->assertEquals(70, $result['followingCount']);
         $this->assertEquals(9, $result['followerCount']);
         $this->assertEquals(false, $result['isFollowed']);
-        $this->assertEquals('/uploads/avatars/my_avatar.jpg', $result['avatarFilepath']);
+        $this->assertEquals('/uploads/avatars/my_avatar.jpg', $result['avatarFilePath']);
         $this->assertEquals('Hello there', $result['bio']);
+    }
+
+    public static function makeUser(): User
+    {
+        return (new User())
+            ->setUsername('test_username')
+            ->setEmail('test_email@test.com')
+            ->setBio('Hello there')
+            ->setAvatarFilename('my_avatar.jpg')
+            ->setPostCount(25)
+            ->setFollowerCount(9)
+            ->setFollowingCount(70);
     }
 
     /**
@@ -74,19 +83,6 @@ class UserNormalizerTest extends KernelTestCase
             ]]
         );
 
-        $this->assertEquals(null, $result['avatarFilename']);
-        $this->assertEquals('/default_avatar.jpg', $result['avatarFilepath']);
-    }
-
-    public static function makeUser(): User
-    {
-        return (new User())
-            ->setUsername('test_username')
-            ->setEmail('test_email@test.com')
-            ->setBio('Hello there')
-            ->setAvatarFilename('my_avatar.jpg')
-            ->setPostCount(25)
-            ->setFollowerCount(9)
-            ->setFollowingCount(70);
+        $this->assertEquals('/default_avatar.jpg', $result['avatarFilePath']);
     }
 }
